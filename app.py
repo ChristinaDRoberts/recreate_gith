bb955a9069829d7f3194994a5ffbb5298247b0f3
Christina <Christinepeninsula@hotmail.com>
2019-01-18 15:26:34 -0500

    progress

--- app.py
+++ app.py
@@ -10,39 +10,55 @@ from flask import render_template
 
 import requests
 
-app = Flask(__name__)   #load all the packages
+#load all the packages
+app = Flask(__name__)   #load all the packages ??? where does it look for packages
 
-
-@app.route("/")  #this says if the search bar shows / insert the info from this function
-def index():      #defining function that will get my info for page from github
+#this says if the search bar shows / , not clicking on tab, insert the info from this function
+@app.route("/", methods=["GET", "POST"])
+# defining function that will get my info for page from github
+def index():
+    # gets info from api and stores it in variable response
     response = requests.get("https://api.github.com/users/ChristinaDRoberts")
+    # gets info and stores it in response 2
     response2 = requests.get("https://api.github.com/users/ChristinaDRoberts/repos")
+    # uses buit in function to read json and convert it to python, stores that in data variable
     data = response.json()
     data2 = response2.json()
 
     # print(data)  # list of dict
     # print(data[1])
 
+    # create a dictionary and assign it a key of repos and a value of the info from second api
     info_repo = {
-        'repos': data2, 'general': data
+        'repos': data2, 'general': data #also assign it another key of general and value of repos info
     }
 
+    # sends the info back to program using render template function
+    # in index file which we will be using for repos page, and replace the {% %} values with
+    # key : value data that we call out of the api data se
     return render_template('index.html', **info_repo)
 
 
-######## how to make tab sent tp followers  ############
+
+
+######## how to make tab sent tp followers , not to self in slack , put in html ############
 
 #############################################################################
 
-@app.route('/followers/', methods=["GET", "POST"]) #if query string has repos included it will GET (using get mehtod)info
-                                                # from api and will use the method POST to pass in info that user types
-                                                # in search bar
+
+#if query string has followers included bc the tab was clicked it will GET (using get mehtod)info from api
+# an api is literally a collection of data and these apis are listsof dictionaries
+
+@app.route('/followers/', methods=["GET"])
+
 def following_people():
     response = requests.get("https://api.github.com/users/ChristinaDRoberts/followers")
     data_followers = response.json()
 
     info_followers = {
         "followers" : data_followers}
+
+    print(data_followers)
 
     return render_template('followers.html', **info_followers)
 
